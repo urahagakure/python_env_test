171790bfaf0c94f5ff3c152af3e1bcbd9ab3da6b
uraha <maebane.0113@gmail.com>
2025-08-28 11:33:57 +0900

    fix: resolve conflict in hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-print("Hello from B" )
-=======
-print("Hello from A" )
->>>>>>> feat/conflict-A
+print("Hello from A")   
 print("Version 2")
 print("branch work")
 print("branch: feat/hello-print")
